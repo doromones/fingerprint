841022bd7f65f7bd041ea6cf5046a8aade8b048c
Anton <doromones@users.noreply.github.com>
2024-02-07 12:57:52 +0200

    add fingerprint

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
 function App() {
+  const [visitorId, setVisitorId] = useState('');
+  const [fingerPrintComponents, setFingerPrintComponents] = useState({});
+
+  async function setFingerprint() {
+      const fp = await FingerprintJS.load();
+      const fingerprint = await fp.get();
+      const { visitorId } = fingerprint;
+      // console.log(fingerprint)
+      setVisitorId(visitorId)
+      setFingerPrintComponents(fingerprint)
+  }
+
+  useEffect(() => {
+      setFingerprint()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h1>This is the fingerprint hash</h1>
+          <h3>VisitorId: {visitorId}</h3>
+
+          <pre>{JSON.stringify(fingerPrintComponents, null, 2)}</pre>
+      </div>
   );
 }
 
